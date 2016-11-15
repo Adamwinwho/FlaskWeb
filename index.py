c9fd4fd3a3fc470fb016b43cc693cd25ee544390
adamwinwho <hs15952016403@gmail.com>
2016-11-15 22:59:32 +0800

    form exercise

--- index.py
+++ index.py
@@ -1,24 +1,35 @@
 from flask import Flask, render_template
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
-
+from flask_moment import Moment
+from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField,SubmitField
+from wtforms.validators import Required
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "hard to guess string"
 manager = Manager(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+class NameForm(FlaskForm):
+    name = StringField("what is your name?",validators=[Required()])
+    submit = SubmitField("Submit")
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    form = NameForm()
+    return render_template("index.html",form=form)
 
 @app.route('/user/<name>')
 def user(name):
-    return render_template("user.html",name=name,bootstrap=bootstrap)
+    return render_template("user.html",name=name)
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template("404.html",bootstrap=bootstrap),404
+    return render_template("404.html"),404
 
 if __name__ == "__main__":
-    #app.run(debug=True)
-    manager.run()
+    app.run(debug=True)
+    #manager.run()
