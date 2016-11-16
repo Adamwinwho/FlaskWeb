cd1853eb13a4facf365c70a6509adafd4d472ae8
adamwinwho <hs15952016403@gmail.com>
2016-11-16 21:43:27 +0800

    use database

--- index.py
+++ index.py
@@ -1,26 +1,62 @@
-from flask import Flask, render_template
-from flask_script import Manager
+import os
+from flask import Flask, render_template,session,redirect,url_for,flash
+from flask_script import Manager,Shell
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
 from flask_wtf import FlaskForm
 from wtforms import StringField,SubmitField
 from wtforms.validators import Required
+from flask_sqlalchemy import SQLAlchemy
 
+basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "hard to guess string"
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///'+os.path.join(basedir,'data.sqlite')
+app.config["SQLALCHEMY_COMMIT_ON_TEARDOWN"] = True
+db = SQLAlchemy(app)
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(64),unique=True)
+    users = db.relationship('User',backref="role")
+
+    def __repr__(self):
+        return "<Role %r>" % self.name
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer,primary_key=True)
+    username = db.Column(db.String(64),unique=True,index=True)
+    role_id = db.Column(db.Integer,db.ForeignKey("roles.id"))
+
+    def __repr__(self):
+        return "<User %r>" % self.username
 
 class NameForm(FlaskForm):
     name = StringField("what is your name?",validators=[Required()])
     submit = SubmitField("Submit")
 
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
 def index():
     form = NameForm()
-    return render_template("index.html",form=form)
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            session["known"] = False
+        else:
+            session["known"] = True
+        session["name"] = form.name.data
+        form.name.data = ""
+        #这里的index就是控制器函数的名称index
+        return redirect(url_for("index"))
+    return render_template("index.html",form=form,name=session.get('name'),known=session.get("known",False))
 
 @app.route('/user/<name>')
 def user(name):
@@ -30,6 +66,11 @@ def user(name):
 def page_not_found(e):
     return render_template("404.html"),404
 
+#为shell命令添加一个上下文
+def make_shell_context():
+    return dict(app=app,db=db,User=User,Role=Role)
+manager.add_command("shell",Shell(make_context=make_shell_context))
+
 if __name__ == "__main__":
-    app.run(debug=True)
-    #manager.run()
+    #app.run(debug=True)
+    manager.run()
